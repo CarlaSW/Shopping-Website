ea393fac50f4582d07837756ced5b3a7988b1c0d
CarlaS <carlasamy200@gmail.com>
2024-09-11 19:16:34 +0300

    new

--- scripts/Homepage.js
+++ scripts/Homepage.js
@@ -1,3 +1,6 @@
+import {cart,addToCart} from'../scripts/cart.js';
+
+
 let productsHtml=``;
 products.forEach((product,index)=>{
     productsHtml+=`
@@ -31,46 +34,4 @@ products.forEach((product,index)=>{
 let showProducts=document.querySelector(".js-products");
 showProducts.innerHTML=productsHtml;
 console.log(productsHtml);
-
-let addToCartButtons=document.querySelectorAll(".js-add-to-cart-button");
-addToCartButtons.forEach((button,index)=>{
-    button.addEventListener('click',()=>{
-        let productId=button.dataset.productId;
-        let matchingItem;
-        cart.forEach((item)=>{
-            if(item.productId===productId)
-            {
-                matchingItem=item;
-            }
-        })
-        if(matchingItem)
-        {
-            matchingItem.quantity++;
-        }
-        else{
-            cart.push({
-                productId:productId,
-                quantity:1
-            })
-        }
-        
-        let totalQuantity=0;
-        cart.forEach((item)=>{
-            totalQuantity+=item.quantity;
-        })
-
-        console.log(cart);
-        console.log(totalQuantity);
-        document.querySelector('.js-cart-quantity').innerHTML=totalQuantity;
-
-        document.querySelectorAll('.js-added').forEach((checkmark)=>{
-            if(checkmark.dataset.productId===productId)
-            {
-                checkmark.style.opacity=1;
-                setTimeout(()=>{
-                    checkmark.style.opacity=0;
-                },1000)
-            }
-        })
-    })
-})
+addToCart();
